57d4731dd1437f20f54f3cc8dedbcbb58fc34a06
Bruno-Ferr <fbruno233@gmail.com>
2022-04-26 11:21:18 -0300

    Created server and user resgistration

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,37 @@
+import { hash } from "bcrypt";
+import { prisma } from "../../../../database/prismaClient";
+
+interface ICreateClient {
+  username: string;
+  password: string;
+}
+
+export class CreateClientUseCase {
+  async execute({ password, username }: ICreateClient) {
+    // User exist verification
+    const clientExist = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: "insensitive"
+        },
+      },
+    });
+
+    if(clientExist){
+      throw new Error("Client already exists");
+    }
+
+    // Encrypt password
+    const hashPassword = await hash(password, 10);
+
+    // Save client
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: hashPassword,
+      },
+    });
+  
+    return client;
+  }
+}
