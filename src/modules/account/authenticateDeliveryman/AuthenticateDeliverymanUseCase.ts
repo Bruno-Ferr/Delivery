9003d2a040ec2fd070e049ea602b91a59fba3cdb
Bruno-Ferr <fbruno233@gmail.com>
2022-04-26 14:54:59 -0300

    deliveryman authentication

---
+++ src/modules/account/authenticateDeliveryman/AuthenticateDeliverymanUseCase.ts
@@ -0,0 +1,40 @@
+import { prisma } from "../../../database/prismaClient";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+interface IAuthenticateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateDeliverymanUseCase {
+  // Recive username, password
+  async execute({ username, password }: IAuthenticateDeliveryman) {
+
+    // Verify if username is registered
+    const deliveryman = await prisma.clients.findFirst({
+      where: {
+        username
+      },
+    });
+
+    if (!deliveryman) {
+      throw new Error("username or password invalid!");
+    }
+    // Verify if password matches username
+    const passwordMatch = await compare(password, deliveryman.password);
+
+    if(!passwordMatch) {
+      throw new Error("username or password invalid!");
+    }
+    // Token generation
+    const token = sign({username}, "898ce53049638f3ff86c4b534cf16078", {
+      subject: deliveryman.id,
+      expiresIn: "1d"
+    });
+
+    return {
+      token,
+    };
+  }
+}
